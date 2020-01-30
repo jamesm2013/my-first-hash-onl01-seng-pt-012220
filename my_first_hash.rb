468e9a737cae40818ce5090b46c6ba4bec705be1
james Johnson <2013matthew2013@gmail.com>
2020-01-30 04:30:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,12 +30,12 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  
+  shipping_manifest["muskets"] = 2
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
 
+ 
 
 
   # return the shipping_manifest hash below
